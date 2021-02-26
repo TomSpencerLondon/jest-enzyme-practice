c205746d72fe49e875f7d87e6eb5a404160ab32d
Tom <thomas.spencer@codurance.com>
2021-02-26 22:42:08 +0000

    Yay! Enzyme is working with @wojtekmaj. Bardzo dobrze!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {getComputation} from "./App";
+import Enzyme from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {shallow} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+const App = require('./App.js')
+
+describe('testComputation', () => {
+  it('adds 1 + 5', () => {
+    expect(getComputation(1, 5)).toBe(6);
+  });
+});
+
+describe("testRender", () =>{
+  it("should render hello world", () => {
+    Enzyme.configure({ adapter: new Adapter() })
+
+    const html = App.sayHello();
+    const wrapper = shallow(html);
+    expect(wrapper.text()).toContain("Hello World!");
+  });
+
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {sayHello} from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <sayHello />
   </React.StrictMode>,
   document.getElementById('root')
 );
